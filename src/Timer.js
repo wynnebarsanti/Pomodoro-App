ea860eda3e27df9a8e021a923fc978d601ee2bdf
Keegan Keane <kk5zr@virginia.edu>
2019-05-29 09:38:11 -0400

    Working Timer that has not yet been formatted with ant-design

--- src/Timer.js
+++ src/Timer.js
@@ -1,46 +1,68 @@
-// this is the timer page
+//The Timer class that contains the logic and display for the timer 
 import React from 'react'
-
 
 class Timer extends React.Component {
     constructor(props)
     {
         super(props);
-        this.state = {display_sec: "00", display_min: "00", secondsRemaining: 60, intervalHandle: ""}
+        this.state = {display_sec: "00", display_min: "25", secondsRemaining: 1500, intervalHandle: ""}
     }
 
-    //this.handleChange = this.handleChange.bind(this);
-    // method that triggers the countdown functionality
-    //this.startCountDown = this.startCountDown.bind(this);
-   // this.tick = this.tick.bind(this);
+    //Binding Prevents async errors from occuring
+    startCountDown = this.startCountDown.bind(this);
+    tick = this.tick.bind(this);
 
-    tick() 
+    //Tick() Function that runs every second once the timer is run 
+    async tick() 
     {
         var min = Math.floor(this.state.secondsRemaining / 60);
-        var sec = this.state.secondsRemaining;
+        var sec = this.state.secondsRemaining - (min *60);
+
+        //Sets the display variables 
+        await this.setState({display_min : min, 
+                       display_sec : sec})
         
-        this.state.display_min = min;
-        this.state.display_sec = sec;
-       
-        if(min === 0 && sec === 0) 
+        const temp = sec;
+
+        //If statement that stops the timer 
+        if(min === 0 && sec === 0)
         {
             console.log("Stopping")
+            this.setState({display_sec : "0" + temp.toString()})
             clearInterval(this.state.intervalHandle);
+            this.setState({display_min: "00", display_sec: "00", secondsRemaining: 1500})
         }
+        //If statement that adjusts display for when the seconds go under 10 
         else if(sec < 10) 
-        {    
+        {
             console.log("seconds is less than 10")  
-           this.state.display_sec = "0" + this.state.seconds;
+            this.setState({display_sec : "0" + temp.toString()})
         }
-
+        //If statement that adjusts display for when the minutes go under 10 
+        if(min < 10)
+        {
+            const temp2 = min
+            console.log("minutes is less than 10")  
+            this.setState({display_min : "0" + temp2.toString()})
+        }
+        //Increments the seconds remaining 
         this.state.secondsRemaining = this.state.secondsRemaining - 1;
         console.log(this.state.secondsRemaining)
-        //this.render();
+        console.log(this.state.display_min)
+        console.log(this.state.display_sec)
+        this.updateDisplay()
     }
 
+    //Starts the tick() function and runs it every second
     startCountDown()
     {
-        this.state.intervalHandle = setInterval(this.tick(), 10);
+        this.state.intervalHandle = setInterval(this.tick, 1000);
+    }
+
+    //Displays the timer
+    updateDisplay()
+    {
+        return (this.state.display_min + ":" + this.state.display_sec)
     }
 
 
@@ -48,7 +70,7 @@ class Timer extends React.Component {
         return(
         <div>
             <button onClick={()=> {this.startCountDown()}}> Start Timer </button>
-            {this.state.display_min}:{this.state.display_sec}
+            {this.updateDisplay()}
         </div>
         );
     }
