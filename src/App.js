fc2b6a5ecae28f18fcc52f17607ff512a3575e0b
Colton Demetriou <eparis@atlassian.com>
2019-05-30 14:50:17 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ import Profile from "./Profile";
 import NewAccount from "./NewAccount";
 import Timer from "./Timer";
 import TimerLog from "./TimerLog"
-import DogImage from "./DogImage.js";
+import TimerBreak from "./TimerBreak"
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 
 class App extends React.Component {
 render(){
-  console.log('Render app')
-  return (
-    <div className="App">
-       <DogImage />
-    </div>
-  );
+ console.log('Render app')
+ return (
+   <div className="App">
+
+      <Router>
+        <Route exact path="/" component={LogIn} />
+        <Route exact path="/NewAccount" component={NewAccount} />
+        <Route exact path="/Profile" component={Profile} />
+        <Route exact path="/Timer" component={Timer} />
+        <Route exact path="/TimerLog" component={TimerLog} />
+        <Route exact path="/TimerBreak" component={TimerBreak} />
+      </Router>
+   </div>
+ );
 }
 
 }
 
 export default App;
-
